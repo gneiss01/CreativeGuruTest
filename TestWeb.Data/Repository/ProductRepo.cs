e438ea6fcba37b5f7658c8bfe794610ea54e5354
Filomeno <gneiss01@gmail.com>
2017-08-08 14:02:38 +0800

    Add ProductRepo class

---
+++ TestWeb.Data/Repository/ProductRepo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using TestWeb.Data.Model;
+using System.Linq;
+
+namespace TestWeb.Data.Repository
+{
+    public class ProductRepo : Repository<Product>
+    {
+        public ProductRepo(DbContext context) : base(context) { }
+
+        public List<Product> GetAll(bool? isActive = null)
+        {
+            return base.GetAll()
+                .Where(p => isActive == null || p.IsActive == isActive.Value)
+                .Select(p => p).ToList();
+        }
+    }
+}
