ed6137761cecc7b2b9048f0ab252bd2176ca2ec3
Filomeno <gneiss01@gmail.com>
2017-08-08 14:03:15 +0800

    Add StoreContext class
    This is the DbContext class for Product and Category

---
+++ TestWeb.Data/StoreContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using TestWeb.Data.Model;
+
+namespace TestWeb.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext() : base("Name=StoreContext")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
