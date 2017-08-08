d36374934829c10ca388dce762bf0a3717914809
Filomeno <gneiss01@gmail.com>
2017-08-08 14:01:12 +0800

    Add Repository class and interface

---
+++ TestWeb.Data/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TestWeb.Data.Repository
+{
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+
+        void Delete(T entity);
+
+        T FindByKey(params object[] keyValues);
+
+        List<T> GetAll();
+    }
+}
---
+++ TestWeb.Data/Repository/Repository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace TestWeb.Data.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected DbContext context = null;
+        protected IDbSet<T> data = null;
+
+        public Repository(DbContext context)
+        {
+            this.context = context;
+            this.data = context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            this.data.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            this.data.Remove(entity);
+        }
+
+        public T FindByKey(params object[] keyValues)
+        {
+            return this.data.Find(keyValues);
+        }
+
+        public List<T> GetAll()
+        {
+            return this.data.ToList();
+        }
+
+        public void Save()
+        {
+            this.context.SaveChanges();
+        }
+    }
+}
