f41d58edf645ad2bf9f9cd59ea6db5fee24f96d5
Filomeno <gneiss01@gmail.com>
2017-08-09 00:39:26 +0800

    Add CustomJsonResult
    This will handle serialization of the controller result.

---
+++ TestWeb/Models/CustomJsonResult.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestWeb.Models
+{
+    public class CustomJsonResult : JsonResult
+    {
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            HttpResponseBase response = context.HttpContext.Response;
+
+            if (!String.IsNullOrEmpty(ContentType))
+            {
+                response.ContentType = ContentType;
+            }
+            else
+            {
+                response.ContentType = "application/json";
+            }
+            if (ContentEncoding != null)
+            {
+                response.ContentEncoding = ContentEncoding;
+            }
+            if (Data != null)
+            {
+                response.Write(JsonConvert.SerializeObject(Data, Formatting.None, new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore }));
+            }
+        }
+    }
+}
