3f4dcda43ed0ee1b4900a01ea041c10f99eec16a
Filomeno <gneiss01@gmail.com>
2017-08-08 19:11:34 +0800

    Add Product listing page

---
+++ TestWeb/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace TestWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
