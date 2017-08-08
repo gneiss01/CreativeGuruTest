eee7db6f04d469fcbccae8c7e68a2109137650c0
Filomeno <gneiss01@gmail.com>
2017-08-08 13:58:35 +0800

    Add Category and Product models

---
+++ TestWeb.Data/Model/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestWeb.Data.Model
+{
+    public class Category
+    {
+        public Category()
+        {
+            this.Products = new List<Product>();
+        }
+
+        public Guid Id { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public virtual List<Product> Products { get; set; }
+    }
+}
---
+++ TestWeb.Data/Model/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestWeb.Data.Model
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public Guid CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
