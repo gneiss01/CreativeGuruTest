c257f068b9b658966d6b9a8a5195f4c987233ed2
Filomeno <gneiss01@gmail.com>
2017-08-09 00:59:32 +0800

    Add search product functionality

--- TestWeb/Scripts/controllers/productController.js
+++ TestWeb/Scripts/controllers/productController.js
@@ -1,5 +1,27 @@
 ﻿app.controller('productController', ['$router', '$scope', '$location', '$rootScope', '$http', '$window', productController]);
 
+app.filter('searchFor', function () {
+    return function (products, searchText) {
+
+        if (!searchText) {
+            return products;
+        }
+
+        var result = [];
+
+        searchText = searchText.toLowerCase();
+        angular.forEach(products, function (product) {
+
+            if (product.DisplayName.toLowerCase().indexOf(searchText) !== -1) {
+                result.push(product);
+            }
+
+        });
+
+        return result;
+    };
+});
+
 function productController($router, $scope, $location, $rootScope, $http, $window) {
     var self = $scope;
 
