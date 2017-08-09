09a92da074d2475a7b92320c53fe42cc4fc409c7
Filomeno <gneiss01@gmail.com>
2017-08-09 19:42:19 +0800

    Add IProductRepo interface

---
+++ TestWeb.Data/Repository/IProductRepo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using TestWeb.Data.Model;
+
+namespace TestWeb.Data.Repository
+{
+    public interface IProductRepo : IRepository<Product>
+    {
+        List<Product> GetAll(bool? isActive = null);
+    }
+}
